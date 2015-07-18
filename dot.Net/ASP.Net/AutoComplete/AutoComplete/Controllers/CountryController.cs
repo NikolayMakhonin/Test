5041b3ee23222db657d15a5b14ec1af248971a15
nikolaymakhonin <nikolay.makhonin@gmail.com>
2015-07-18 21:43:43 +0700

    AutoComplete WebApi Complete

---
+++ dot.Net/ASP.Net/AutoComplete/AutoComplete/Controllers/CountryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Caching;
+using System.Web.Http;
+using System.Web.Http.Results;
+using AutoComplete.Models;
+
+namespace AutoComplete.Controllers
+{
+    public class CountryController : ApiController
+    {
+        // GET: api/Country/GetCountries?firstLetters=...
+        public JsonResult<IEnumerable<Country>> GetCountries(string firstLetters)
+        {
+            return Json<IEnumerable<Country>>(CountriesContext.Instance.GetCountries(firstLetters));
+        }
+    }
+}
